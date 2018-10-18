01436d4255454cd31fe760221e87c80ab394a863
Mona Lisa <estreadbeck@estreadbeck-l.corp.instructure.com>
2018-10-18 11:19:21 -0600

    added a seeds file

--- Gemfile
+++ Gemfile
@@ -61,3 +61,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'devise', '~> 4.5'
 gem 'friendly_id', '~> 5.2', '>= 5.2.4'
+gem 'faker', '~> 1.9', '>= 1.9.1'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+topic_selection = ["#{Faker::Hacker.unique.noun}", "#{Faker::Food.unique.dish}", "#{Faker::Company.unique.name}","#{Faker::Currency.unique.name}","#{Faker::File.unique.extension}","#{Faker::FunnyName.unique.name}"]
+
+
+puts "Starting to run seeds file..."
+
+100.times do |topic|
+  Topic.create!(title: "#{topic_selection.sample}")
+end
+
+puts "100 Topics created"
+
+User.create!(
+  email: "#{Faker::Internet.unique.email}",
+  password: "asdfasdf",
+  password_confirmation: "asdfasdf",
+  first_name: "#{Faker::Name.unique.first_name}",
+  last_name: "#{Faker::Name.unique.last_name}",
+  username: "Adminanator",
+  role: "admin"
+)
+
+puts "Admin user created"
+
+User.create!(
+  email: "#{Faker::Internet.unique.email}",
+  password: "asdfasdf",
+  password_confirmation: "asdfasdf",
+  first_name: "#{Faker::Name.unique.first_name}",
+  last_name: "#{Faker::Name.unique.last_name}",
+  username: "studentman",
+  role: "student"
+)
+
+puts "Student user created"
